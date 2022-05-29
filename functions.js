40043d92dd55c99376ed929a2f79906535c5c102
Yannic Holländer <yannic.hollaender@gmail.com>
2022-05-29 13:10:21 +0200

    restrcutured code

--- functions.js
+++ functions.js
@@ -1,96 +1,106 @@
+let stateArray = new Array();
 let containers = document.getElementsByClassName('logo_container');
-let isMouseOver = false;
-let isThrottled = false;
 let docStyle = getComputedStyle(document.documentElement);
 let fadeDuration = docStyle.getPropertyValue('--fade-duration'); 
-let stateArray = new Array();
+let rotateDuration = docStyle.getPropertyValue('--rotate-duration'); 
 
-
-// parse fade-duration (could be ms or s)
-(fadeDuration.indexOf('ms') !== -1) ? 
-    fadeDuration = parseFloat(fadeDuration.replace('ms', '')) : 
-    fadeDuration = parseFloat(fadeDuration.replace('s', '')) * 1000
-  
-
-// throttle-Decorator: When wrapper is called multiple times, it passes the  
-// call to f at maximum once per ms milliseconds. Here, all throttle decorators 
-// use the global "isThrottled" variable.
-function throttle(func, ms, idx) {
-    
-        function wrapper() {
-            if (isThrottled) {
-            return;
-            }
-            isThrottled = true;
-
-            func.apply(this, arguments);
-
-            setTimeout(function() {
-                isThrottled = false;
-            }, ms);
-        }
-
-    return wrapper;
+// parse css durations (could be ms or s)
+function parseDuration (d) {
+    return (d.indexOf('ms') !== -1) ? 
+        parseFloat(d.replace('ms', '')) : 
+        parseFloat(d.replace('s', '')) * 1000;
 }
-
-function switchAnimation (classListWhite, classListColor) {
-    if (!( 
-        // check for any status classes on child elements
-        classListWhite.contains('active') || 
-        classListWhite.contains('inactive') || 
-        classListColor.contains('active') || 
-        classListColor.contains('inactive') 
-    )) { 
-        // on first click initiate animation states (active, inactive) 
-        classListWhite.add('inactive');
-        classListColor.add('active');
-    } else {
-        // on subsequent clicks, toggle animation states
-        classListWhite.toggle('active');
-        classListColor.toggle('active');
-        classListWhite.toggle('inactive');
-        classListColor.toggle('inactive');
-    }
-
-    // always remove rotate class for smooth finish
-    classListWhite.remove('rotate');
-    classListColor.remove('rotate');
-
-    // if mouse still hovers after animation -> restart rotation
-    setTimeout(() => {
-        if (isMouseOver) {
-            classListWhite.add('rotate');
-            classListColor.add('rotate');
-        }
-    }, fadeDuration)
-}
-
-function forwardRotation(classListWhite, classListColor) {
-    classListWhite.add('rotate');
-    classListColor.add('rotate');
-}
-
-function backwardRotation(classListWhite, classListColor) {
-    classListWhite.remove('rotate');
-    classListColor.remove('rotate');
-}
+fadeDuration = parseDuration(fadeDuration);
+rotateDuration = parseDuration(rotateDuration);
 
 
-switchAnimation = throttle(switchAnimation, fadeDuration);
-forwardRotation = throttle(forwardRotation, 0);
-backwardRotation = throttle(backwardRotation, 0);
 
-
+// use class for independent event tracking of every container/icon
 class ContainerState {
-    contructor() {
-        this.isMouseOver = false;
-        this.isThrottled = false;
+    isMouseOver = false;
+    isThrottled = false; 
+
+    forwardRotation(classListWhite, classListColor) {
+        this.isMouseOver = true;
+        if (this.isThrottled) {
+            return;
+        }
+        // rotate logo by adding class
+        classListWhite.add('rotate');
+        classListColor.add('rotate');
+        
+        // always complete full rotation (throttle until then)
+        this.isThrottled = true;
+        setTimeout(() => {
+            // initiate backwardRotation if mouseout-event occured while completing forwardRotation
+            this.isThrottled = false;
+            if (!this.isMouseOver) {
+                this.backwardRotation(classListWhite, classListColor) 
+            }
+        }, rotateDuration);
+
     }
+
+    backwardRotation(classListWhite, classListColor) {
+        this.isMouseOver = false;
+        if (this.isThrottled) {
+            return;
+        }
+        // rotate back by removing class
+        classListWhite.remove('rotate');
+        classListColor.remove('rotate');
+        
+    }
+
+    switchAnimation (classListWhite, classListColor) {
+        //throttling is to prevent double clicking etc. to restart the animation
+        if (this.isThrottled) {
+            return;
+        }
+        this.isThrottled = true;
+
+        if (!( 
+            // check for any status classes on child elements
+            classListWhite.contains('active') || 
+            classListWhite.contains('inactive') || 
+            classListColor.contains('active') || 
+            classListColor.contains('inactive') 
+        )) { 
+            // on first click initiate animation states (active, inactive) 
+            classListWhite.add('inactive');
+            classListColor.add('active');
+        } else {
+            // on subsequent clicks, toggle animation states
+            classListWhite.toggle('active');
+            classListColor.toggle('active');
+            classListWhite.toggle('inactive');
+            classListColor.toggle('inactive');
+        }
+    
+        // always remove rotate to prevent jitter at animation finish
+        classListWhite.remove('rotate');
+        classListColor.remove('rotate');
+    
+        // if mouse still hovers after animation -> restart rotation
+        setTimeout(() => {
+            if (this.isMouseOver) {
+                classListWhite.add('rotate');
+                classListColor.add('rotate');
+            }
+        }, fadeDuration)
+
+        setTimeout(() => {
+            this.isThrottled = false;
+        }, fadeDuration);
+
+    }
+
 }
 
 // set up event listeners for all logo containers
 Array.from(containers).forEach((c, idx) => {
 
+    // one containerState object for every logo/container
     stateArray.push(
         new ContainerState()  
     )
@@ -100,19 +110,19 @@ Array.from(containers).forEach((c, idx) => {
 
     // mouseover-event initiates rotation 
     c.addEventListener('mouseover', ()=>{
-        forwardRotation(classes_w, classes_c, idx)
-        isMouseOver = true;
+        stateArray[idx].forwardRotation(classes_w, classes_c)
+
         })
 
     // mouseout-event reverses rotation
     c.addEventListener('mouseout', ()=>{
-        backwardRotation(classes_w, classes_c, idx)
-        isMouseOver = false;
+        stateArray[idx].backwardRotation(classes_w, classes_c)
+
         })
 
     // click-event initiates animations by switching state classes
     c.addEventListener('click', ()=>{
-        switchAnimation(classes_w, classes_c, idx)
+        stateArray[idx].switchAnimation(classes_w, classes_c)
         })
 
     })
